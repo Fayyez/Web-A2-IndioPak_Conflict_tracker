682464b6d5c2a09f42550d5c631adfee82a2f733
fayyez <fayyez2056@gmail.com>
2025-05-10 03:09:08 +0500

    added routes

--- express/index.js
+++ express/index.js
@@ -1,1 +1,14 @@
 // this is the main express server
+
+import { config } from "dotenv";
+config();
+
+import {countryRouter} from "./routes/countryRoutes.js";
+import {missileRouter} from "./routes/missleRoutes.js";
+import express from "express";
+import mongoose from "mongoose";
+
+// confirgue db
+// create server
+// set middleware
+// stasrt server
